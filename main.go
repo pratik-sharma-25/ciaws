bca2bd53b67e6315989e1bb089a6f097626e8b80
pratik-sharma-25 <pratik.sharma@brevo.com>
2025-06-29 10:44:28 +0530

    test deployment

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World!"))
+		w.Write([]byte("Hello, World! Pratik"))
 	})
 	server := &http.Server{
 		Addr:    ":8070",
