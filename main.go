6726fc429fcaffc922d975d8221f4cd435eea960
pratik-sharma-25 <pratik.sharma@brevo.com>
2025-06-29 11:01:36 +0530

    pushing aws without runner

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World! Pratik Sharma AWS"))
+		w.Write([]byte("Hello, World! Pratik Sharma AWS without any changes"))
 	})
 	server := &http.Server{
 		Addr:    ":8070",
