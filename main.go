8a3b13470d2f4c5eda0cf53d891bf023b875cc38
Pratik Sharma <pratik.sharma@brevo.com>
2025-06-30 18:12:36 +0530

    self hosted runner

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main() {
 	router := http.NewServeMux()
 	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello, World! Pratik Sharma AWS without any changes"))
+		w.Write([]byte("Hello, World! Pratik Sharma AWS from self hosted runner"))
 	})
 	server := &http.Server{
 		Addr:    ":8070",
